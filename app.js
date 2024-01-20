c043aea55437b5198e71ad06019227e04135c5a4
megha.chhabra <megha.chhabra@techangouts.com>
2024-01-21 04:22:52 +0530

    added react and setup

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const heading1 = React.createElement("h1",{id:'helloworld', class:'heading' },"Hello world 1");
-const heading2 = React.createElement("h1",{id:'name', class:'heading' },"Hello name");
-
-
+const heading1 = React.createElement(
+  "h1",
+  {
+    id: 'helloworld',
+    className: 'heading',
+    key:'01'
+  },
+  "Hello world 1"
+);
+const heading2 = React.createElement("h1", { id: 'name', className: 'heading',  key:'2' }, "Hello name");
 
 const container = React.createElement(
- "div",
-    {
-      id: "container",
-    },
-    [heading1, heading2]
-  );
- 
-  //passing a react element inside the root 
-  //async defer
+  "div",
+  {
+    id: "container",
+  },
+  [heading1, heading2]
+);
+
+//passing a react element inside the root 
+//async defer
 
 root.render(container);
 
 
 
-let a = (i)=>{
-    console.log('ho')
-}
